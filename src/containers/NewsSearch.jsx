096c7fd9189ad58383343eaadf5b518aae1ce77f
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-03 16:19:44 -0700

    fetch articles working, next passing down through props

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,17 +1,32 @@
+// NewsSearch Container
 import React, { Component } from 'react';
 import ArticleList from '../components/articles/ArticleList';
 import Search from '../components/articles/Search';
+import { fetchArticles } from '../services/NewsAPI';
 
 class NewsSearchContainer extends Component {
   state = {
-    loading = 'true',
-    articles = [],
+    loading: 'true',
+    articles: [],
+  };
+
+  async componentDidMount() {
+    const articles = await fetchArticles();
+    this.setState({ articles, loading: false });
   }
   render() {
+    const { loading, articles } = this.state;
+
     return (
       <>
-        <Search />
-        <ArticleList />
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <Search />
+            <ArticleList articles={articles} />
+          </>
+        )}
       </>
     );
   }
--- src/services/NewsAPI.js
+++ src/services/NewsAPI.js
@@ -1,6 +1,6 @@
 export const fetchArticles = async () => {
   const res = await fetch(
-    `https://newsapi.org/v2/everything?q=math&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=20`
+    `https://newsapi.org/v2/everything?q=strike&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=20`
   );
   const articles = await res.json();
   console.log('articles', articles.articles);
